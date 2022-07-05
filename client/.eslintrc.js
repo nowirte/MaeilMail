1029aa6b27e52373937817df9199dbf100f3964e
ouivve <ouivve@gmail.com>
2022-07-05 22:45:59 +0900

    [#2] setup: ESLint 설정

---
+++ client/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021ß: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+};
