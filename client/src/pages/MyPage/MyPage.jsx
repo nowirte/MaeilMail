4ebe91d0dbe0da8815bc56f570bc8686539954e9
jaeyeong815 <jae0ee@naver.com>
2022-07-07 01:34:49 +0900

    grid&flex 적용하여 프로필영역 1차 완성

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -1,60 +1,98 @@
 import React from 'react';
 import styled from 'styled-components';
 import defaultUserIcon from '../../assets/defaultUserIcon.png';
+import settingIcon from '../../assets/settingIcon.png';
 
 const MyPage = () => {
+  const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    display: grid;
+    grid-template-columns: 300px 2fr;
+    grid-template-rows: 100px 1fr 1fr;
+  `;
+
   const Profile = styled.div`
-    background-color: red;
     display: flex;
     flex-direction: column;
+    justify-content: flex-start;
     align-items: center;
-    justify-content: 0 center;
 
-    .UserInfo {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
+    grid-row: 2/3;
 
-    img {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
+    > img {
       width: 100px;
       height: 100px;
       background-color: #ffe5b8;
       border-radius: 50%;
 
-      margin-bottom: 15px;
+      margin: 20px 0;
     }
 
     .nickName {
       font-weight: bold;
-      font-size: 25px;
+      font-size: x-large;
+
+      padding: 10px 0;
     }
 
     .introduction {
-      font-size: 16px;
+      font-size: large;
+      padding: 0 0 20px 0;
+    }
+
+    .profileEdit,
+    .userEdit {
+      display: flex;
+      align-items: center;
+
+      font-size: small;
+      color: gray;
+
+      padding-bottom: 5px;
+
+      > img {
+        width: 15px;
+        height: 15px;
+        margin-right: 5px;
+      }
+
+      > p {
+        ${'' /* 텍스트와 톱니바퀴 중앙 정렬 필요! */}
+      }
     }
   `;
 
   const CurrentStudy = styled.div`
     background-color: blue;
+
+    grid-row: 2 / 4;
+
+    div {
+    }
   `;
 
   return (
-    <div style={{ display: 'flex' }}>
+    <Container>
+      <div>nav 임시 | 높이 100px</div>
       <Profile>
         <img src={defaultUserIcon} alt="profileImg" />
-        <div className="UserInfo">
-          <div className="nickName">갓생살자</div>
-          <div className="introduction">스터디 100%참여가 목표입니다.</div>
+        <div className="nickName">갓생살자</div>
+        <div className="introduction">스터디 100%참여 목표!</div>
+        <div className="profileEdit">
+          <img src={settingIcon} alt="settingImg" />
+          <div>프로필 수정</div>
+        </div>
+        <div className="userEdit">
+          <img src={settingIcon} alt="settingImg" />
+          <div>회원정보 변경</div>
         </div>
       </Profile>
-      <CurrentStudy>study</CurrentStudy>
-    </div>
+      <CurrentStudy>
+        <div>study</div>
+      </CurrentStudy>
+    </Container>
   );
 };
 
