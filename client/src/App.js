69980785e50de46a76df399308438e78c66faea9
jaeyeong815 <jae0ee@naver.com>
2022-07-06 15:06:47 +0900

    [#13] setup: MyPage(/mypage) 페이지 라우팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import MyPage from './pages/MyPage/MyPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </Router>
   );
