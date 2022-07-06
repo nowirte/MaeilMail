ada1e7427cc04585a41cfd9f5e3416aaea82c9e1
jaeyeong815 <jae0ee@naver.com>
2022-07-06 23:37:52 +0900

    마이페이지 프로필/스터디 영역 분리

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
+import defaultUserIcon from '../../assets/defaultUserIcon.png';
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  const Profile = styled.div`
+    background-color: red;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: 0 center;
+
+    .UserInfo {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+
+    img {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      width: 100px;
+      height: 100px;
+      background-color: #ffe5b8;
+      border-radius: 50%;
+
+      margin-bottom: 15px;
+    }
+
+    .nickName {
+      font-weight: bold;
+      font-size: 25px;
+    }
+
+    .introduction {
+      font-size: 16px;
+    }
+  `;
+
+  const CurrentStudy = styled.div`
+    background-color: blue;
+  `;
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <Profile>
+        <img src={defaultUserIcon} alt="profileImg" />
+        <div className="UserInfo">
+          <div className="nickName">갓생살자</div>
+          <div className="introduction">스터디 100%참여가 목표입니다.</div>
+        </div>
+      </Profile>
+      <CurrentStudy>study</CurrentStudy>
+    </div>
+  );
 };
 
 export default MyPage;
