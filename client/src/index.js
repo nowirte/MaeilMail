fefbe6096d712e8454b9293d9cd2486828ab8dcd
ouivve <ouivve@gmail.com>
2022-07-07 18:23:16 +0900

    [#20] style: global style 설정(reset css, font family)

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
 import App from './App';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap');
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
