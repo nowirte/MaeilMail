4ab981c708b4250f177e87716cf37ea808e8f46a
janghan <bjh4135@gmail.com>
2022-07-08 16:52:38 +0900

    [#5, #22] setup: db, swagger 세팅

--- server/src/database/config/db.config.js
+++ server/src/database/config/db.config.js
@@ -1,12 +1,11 @@
-const maria = require("mysql");
+import mysql from 'mysql'
 
-const db = maria.createConnection({
+const db = mysql.createConnection({
     host: process.env.RDS_HOSTNAME,
     user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
     database: process.env.RDS_DB_NAME,
     port: process.env.RDS_PORT, 
 });
-
 
 export { db };
