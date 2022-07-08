8bd20bb0e3c2fb92ae619fddbffe8016a7ba1aca
janghan <bjh4135@gmail.com>
2022-07-08 19:21:53 +0900

    [#5] setup: backend prettier, eslinst

---
+++ server/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'arrow-body-style': ['error', 'always'],
+    'import/prefer-default-export':'off',
+  },
+};
