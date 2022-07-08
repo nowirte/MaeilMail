7f3a969fbbdd8d11b28532f9718dfccf6251018e
JuHyeokLee <bjuhyeok@gmail.com>
2022-07-08 13:55:24 +0900

    [#25] setup: 회원관리 페이지 라우팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import AdminManageAccount from './pages/AdminManageAccount';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/AdminManageAccount" element={<AdminManageAccount />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/AdminManageAccount/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AdminManageAccount = () => {
+  return <div>admin</div>;
+};
+
+export default AdminManageAccount;
