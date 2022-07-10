1bc44b8d74825a7c9336c3069f15c779aff20da7
janghan <bjh4135@gmail.com>
2022-07-10 15:42:38 +0900

    [#5] docs: 불필요 코드 제거

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv'
-// const dotenv = require("dotenv");
 import path from 'path';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// import passport from 'passport';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs'
 
@@ -18,14 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// passportConfig();
-
 app.get("/", (req, res) => res.send("express!"));
-// app.use(passport.initialize());
 
-// app.use("/api/user", userRouter);
-// app.use("/api/study", studyRouter);
+app.use("user", userRouter);
+app.use("study", studyRouter);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'))
 
 app.use(
