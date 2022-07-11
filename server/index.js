1b0a5f6b08bdffd61d30edb72b77c6dfee1c8447
janghan <bjh4135@gmail.com>
2022-07-11 22:33:59 +0900

    setup: babel-node 세팅

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import { app } from './src/app.js';
 import { db } from './src/db/config/db.config.js'
 
--- server/src/app.js
+++ server/src/app.js
@@ -3,17 +3,18 @@ import express from 'express';
 import dotenv from 'dotenv'
 import passport from 'passport';
 import passportConfig from './passport';
-import { loginRouter } from './routers'
-
+import { loginRouter } from './routers/login-router'
+passportConfig();
 dotenv.config()
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-passportConfig();
 app.use(passport.initialize());
+
 app.use('/login', loginRouter)
 
 export { app };
