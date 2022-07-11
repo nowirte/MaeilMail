3db0efd5c6e81538b0fafd99630dbb187d07faf1
jaeyeong815 <jae0ee@naver.com>
2022-07-11 14:33:42 +0900

    fix: app.js에서 home import 코드 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SideBar from './components/sidebar';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Friend from './pages/friend';
 import SignUp from './pages/signup';
 
