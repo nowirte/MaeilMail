6a7fefae08911074c687feedc92bc66cf8b75f06
janghan <bjh4135@gmail.com>
2022-07-11 16:03:05 +0900

    setup: without swagger

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,6 @@
 import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv'
-import path from 'path';
-import { fileURLToPath } from 'url';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs'
 
 dotenv.config()
 const app = express();
@@ -18,14 +14,5 @@ app.get("/", (req, res) => res.send("express!"));
 app.use("user", userRouter);
 app.use("study", studyRouter);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'))
-
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, { explorer: true })
-);
 
 export { app };
