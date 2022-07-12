457618457298d509e46aba5c4c474eaaea5de6d9
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-07-12 16:27:50 +0900

    [#42] feat: 메인 컴포넌트 구현

---
+++ client/src/pages/Home/RecentlyArrivedHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StyledRecentlyArrivedHeader from './styles/StyledRecentlyArrivedHeader';
+
+const RecentlyArrivedHeader = () => {
+  return <StyledRecentlyArrivedHeader>최근 도착</StyledRecentlyArrivedHeader>;
+};
+
+export default RecentlyArrivedHeader;
