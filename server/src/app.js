6b5c825de25e32c022b5fdfc832986eea470fdb6
KMK_HM <kyun9151@gmail.com>
2022-07-12 11:24:46 +0900

    [#5] feat: mysql2 패키지를 이용한 비동기 처리 적용

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import express from 'express';
-import dotenv from 'dotenv'
-
+import dotenv from 'dotenv';
+import {db} from './db/config/db.config.js';
 dotenv.config()
 const app = express();
 
@@ -10,5 +10,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.send("express!"));
+app.get("/user/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const sql = `SELECT * FROM user WHERE user_id = ${id};`;
+        const [user] = await db.promise().query(sql);
+        console.log(user[0].nickname);
+        res.json(user);    
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 export { app };
