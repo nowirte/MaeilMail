7b16ab6064c9addf44294cc3cc078fb9095dc56a
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-07-16 14:18:40 +0900

    docs: 친구추천  상세페이지 코드 수정

--- client/src/pages/Home/RecommendFriendArea.jsx
+++ client/src/pages/Home/RecommendFriendArea.jsx
@@ -1,4 +1,5 @@
 import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
 import CakeOutlinedIcon from '@mui/icons-material/CakeOutlined';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import React from 'react';
@@ -13,8 +14,23 @@ import {
 } from './styles/StyledRecommendFriendArea';
 
 export const RecommendFriendArea = props => {
-  const { gender, birthday, location, profileText, email, nickname } =
-    props.data;
+  const {
+    gender,
+    birthday,
+    location,
+    profileText,
+    email,
+    nickname,
+    favor,
+    language,
+  } = props.data;
+
+  // console.log(favor, 'favor');
+
+  const selectedFavor = favor.filter(e => e.selected == true);
+  const selectedLanguage = language.filter(e => e.selected == true);
+
+  // console.log(selectedFavor);
   return (
     <Friend>
       <Account>
@@ -23,10 +39,7 @@ export const RecommendFriendArea = props => {
       </Account>
       <Info>
         <div className="gender">
-          <p>
-            <FemaleIcon />
-            {gender}
-          </p>
+          <p>{gender === 'men' ? <MaleIcon /> : <FemaleIcon />}</p>
         </div>
         <div className="birthday">
           <p>
@@ -47,11 +60,19 @@ export const RecommendFriendArea = props => {
       <AdditionalInfo>
         <InterestInfo>
           <p className="title">관심사</p>
-          <p className="tag">스포츠</p>
+          {selectedFavor.map(item => (
+            <p className="tag" key={item.value}>
+              {item.value}
+            </p>
+          ))}
         </InterestInfo>
         <LanguageInfo>
           <p className="title">언어</p>
-          <p className="tag">한국어</p>
+          {selectedLanguage.map(item => (
+            <p className="tag" key={item.value}>
+              {item.value}
+            </p>
+          ))}
         </LanguageInfo>
       </AdditionalInfo>
     </Friend>
