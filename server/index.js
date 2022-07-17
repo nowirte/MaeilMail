27b69cce681d305af891fe18e9518b8307d0cb29
janghan <bjh4135@gmail.com>
2022-07-17 23:32:03 +0900

    fix: // 삭제

--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ import 'dotenv/config';
 import { app } from './src/app';
 
 const PORT = process.env.PORT || 3000;
-// db.connect(error => {
-//     if (error) throw error
-//     console.log("DB 연결 성공"); 
-// })
+
 app.listen(PORT, () => {
     console.log(`서버 정상 실행 http://localhost:${PORT}`)
 })
