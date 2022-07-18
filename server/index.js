f72ce74d6bb5833787c6620c9b0ad45703d26bf4
janghan <bjh4135@gmail.com>
2022-07-18 13:57:42 +0900

    setup: concurrently

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { app } from './src/app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`서버 정상 실행 http://localhost:${PORT}`)
