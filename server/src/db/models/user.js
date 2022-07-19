bddf0d33bb3bcefba7c84b8eaf17a78acff4a3cd
janghan <bjh4135@gmail.com>
2022-07-20 00:35:23 +0900

    temp

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -31,7 +31,7 @@ class User extends Sequelize.Model {
       },
 
       birthday: {
-        type: DataTypes.DATEONLY,
+        type: DataTypes.DATE,
       },
 
       location: {
@@ -78,6 +78,7 @@ class User extends Sequelize.Model {
 
   static associate(db) { 
     db.User.hasOne(db.Favor, { foreignKey: 'userId', sourceKey: 'user_id', onDelete: 'cascade', onUpdate: 'cascade' });
+    db.User.hasOne(db.Language, { foreignKey: 'userId', sourceKey: 'user_id', onDelete: 'cascade', onUpdate: 'cascade' })
     db.User.hasMany(db.Letter, { foreignKey: 'sendId', sourceKey: 'user_id', onDelete: 'cascade', onUpdate: 'cascade' });
     db.User.hasMany(db.Letter, { foreignKey: 'receiveId', sourceKey: 'user_id', onDelete: 'cascade', onUpdate: 'cascade' });
    }
