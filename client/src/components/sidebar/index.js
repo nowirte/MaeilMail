0c601a74c56aa11b389f511839f2ef977f24f2ec
jaeyeong815 <jae0ee12@gmail.com>
2022-07-19 13:52:50 +0900

    style: 친구리스트 받아오는 데이터 index에서 분리

--- client/src/components/sidebar/index.js
+++ client/src/components/sidebar/index.js
@@ -1,46 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router';
 import NavTopArea from './NavTop';
 import NavFriendsArea from './NavFriends';
 import LogoutArea from './Logout';
 import { Container, SideWrapper, Navbar } from './style';
-import axios from 'axios';
 
 const SideBar = () => {
-  // const [user, setUser] = useState([]);
-  const [friends, setFriends] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const res = await axios.get(`http://localhost:3333/friend`);
-      const data = res.data;
-
-      // 추후 친구 컴포넌트에서 get할 예정
-      // const res = await axios.get(`http://localhost:3001/api/auth/me`, {
-      //   headers: {
-      //     'Content-Type': 'application/json; charset=utf-8',
-      //     authorization: `Bearer ${localStorage.getItem('token')}`,
-      //   },
-      // });
-      // console.log(res);
-
-      setFriends(data);
-    } catch (err) {
-      console.log(err);
-      console.log(err.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <Container>
       <SideWrapper>
         <Navbar>
           <NavTopArea />
-          <NavFriendsArea friends={friends} />
+          <NavFriendsArea />
         </Navbar>
         <LogoutArea />
       </SideWrapper>
