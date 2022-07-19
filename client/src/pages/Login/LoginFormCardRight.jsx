13d5a0b16f273e3c17d5355a233d668ba60aaf76
JuHyeokLee <bjuhyeok@gmail.com>
2022-07-19 11:21:50 +0900

    <#57> feat: 로그인 토큰 저장 후 리다이렉트

--- client/src/pages/Login/LoginFormCardRight.jsx
+++ client/src/pages/Login/LoginFormCardRight.jsx
@@ -54,7 +54,6 @@ const LoginForm = () => {
     const data = { email: email, password: password };
     const bodyData = JSON.stringify(data);
 
-    //아직 백엔드와 연결 X
     await axios
       .post('http://localhost:3001/api/auth/login', bodyData, {
         headers: {
@@ -62,8 +61,30 @@ const LoginForm = () => {
         },
       })
       .then(function (response) {
-        console.log(response);
+        const { role, token } = response.data;
+        console.log(`${role}, ${token}`);
+        localStorage.setItem('token', token);
         navigate('/');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    console.log(state);
+  }
+
+  async function handleGoogleLoginClick(e) {
+    e.preventDefault();
+
+    //CORS에러 있음
+    await axios
+      .get('http://localhost:3001/api/auth/login/google', {
+        headers: {
+          'Content-Type': 'application/json',
+          withCredentials: true,
+        },
+      })
+      .then(function (response) {
+        console.log(response);
       })
       .catch(function (error) {
         console.log(error);
@@ -91,7 +112,7 @@ const LoginForm = () => {
         <Link to="/home">비밀번호 찾기</Link>
         <Link to="/signup">회원가입</Link>
       </LinkContainer>
-      <LoginGoogleButton>
+      <LoginGoogleButton onClick={handleGoogleLoginClick}>
         <GoogleLogo src={googleLogo} alt="googleLogo" />
         Google
       </LoginGoogleButton>
