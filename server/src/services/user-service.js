2575a91c7529d3a3c471c62b9ec7c499467fa327
janghan <bjh4135@gmail.com>
2022-07-20 22:05:28 +0900

    [#53] docs: function in utils

--- server/src/services/user-service.js
+++ server/src/services/user-service.js
@@ -2,6 +2,7 @@
 import { Op, Sequelize } from 'sequelize';
 import bcrypt from 'bcrypt';
 import { User, Favor, Language } from '../db/models';
+import { getArrayForInputTag, getObjectForDB } from '../utils'
 
 const include = [
   { model: Favor, attributes: { exclude: ['favor_id', 'userId', 'createdAt', 'updatedAt'] } },
@@ -55,10 +56,10 @@ class UserService {
 
     const newUser = await this.User.create(newUserInfo);
 
-    const userId = newUser.dataValues.user_id
+    const userId = newUser.dataValues.user_id;
 
-    await this.Favor.create({userId})
-    await this.Language.create({userId})
+    await this.Favor.create({ userId });
+    await this.Language.create({ userId });
 
     return newUser;
   }
@@ -74,10 +75,10 @@ class UserService {
 
     const newUser = await this.User.create(userInfo);
 
-    const userId = newUser.dataValues.user_id
+    const userId = newUser.dataValues.user_id;
 
-    await this.Favor.create({userId})
-    await this.Language.create({userId})
+    await this.Favor.create({ userId });
+    await this.Language.create({ userId });
 
     return newUser;
   }
@@ -136,16 +137,8 @@ class UserService {
     }
 
     // 회원 정보 수정
-    async function createObject(array) {
-      const result = {};
-      await array.forEach(el => {
-        result[el.value] = el.selected;
-      });
-      return result;
-    }
-
-    const languageUpdate = await createObject(language);
-    const favorUpdate = await createObject(favor);
+    const languageUpdate = await getObjectForDB(language);
+    const favorUpdate = await getObjectForDB(favor);
 
     const hashedPassword = newPassword ? await bcrypt.hash(newPassword, 10) : null;
 
@@ -232,27 +225,14 @@ class UserService {
     if (!user) {
       throw new Error('404 not found');
     }
-    
-    const favObj = user.dataValues.Favor
-    const langObj = user.dataValues.Language
 
-    function getArray(obj) {
-      const result = []
-      const keys = Object.keys(obj)
-      
-      keys.forEach(key => {
-        const element = {}
-        element.value = key
-        element.label = key.charAt(0).toUpperCase() + key.slice(1)
-        element.selected = obj[key]
-        result.push(element);
-      })
-      return result
-    }
-    const favorArray = favObj ? getArray(favObj.dataValues) : null
-    const languageArray = langObj? getArray(langObj) : null
+    const favObj = user.dataValues.Favor;
+    const langObj = user.dataValues.Language;
 
-    return {favorArray, languageArray, user}
+    const favorArray = favObj ? getArrayForInputTag(favObj.dataValues) : null;
+    const languageArray = langObj ? getArrayForInputTag(langObj.dataValues) : null;
+
+    return { favorArray, languageArray, user };
   }
 
   async getUsersBySearch(nickname) {
--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -1,2 +1,4 @@
-export * from './setUserToken'
-export * from './multer'
+export * from './setUserToken';
+export * from './multer';
+export * from './getArrayForInputTag';
+export * from './getObjectForDB';
