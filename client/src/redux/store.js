8d90c76296d9f53c57a200696453b3d4e9c54bd0
JuHyeokLee <bjuhyeok@gmail.com>
2022-07-21 17:19:34 +0900

    feat:reduxpersist 셋팅

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,34 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers/index';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
