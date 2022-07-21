f48f2b6651b1639d7f777798ba51363da1b390d9
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-07-21 21:58:44 +0900

    docs: redux-persist에 맞춰 코드 수정

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,12 @@
 import authReducer from './auth';
 import { combineReducers } from '@reduxjs/toolkit';
+import searchUserSlice from './searchUser';
+import mainLettersSlice from './mainLetters';
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  searchUser: searchUserSlice.reducer,
+  mainLetters: mainLettersSlice.reducer
 });
 
 export default rootReducer;
