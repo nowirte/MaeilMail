4fc07fa53c5ea51b6e001c074da39024102409e1
janghan <bjh4135@gmail.com>
2022-07-23 00:01:36 +0900

    let google go

--- client/src/pages/Login/LoginFormCardRight.jsx
+++ client/src/pages/Login/LoginFormCardRight.jsx
@@ -49,7 +49,7 @@ const LoginForm = props => {
     const bodyData = JSON.stringify(data);
 
     await axios
-      .post('34.64.149.17:80/api/auth/login', bodyData, {
+      .post('/api/auth/login', bodyData, {
         headers: {
           'Content-Type': 'application/json',
         },
@@ -71,7 +71,7 @@ const LoginForm = props => {
 
     //CORS에러 있음
     await axios
-      .get('34.64.149.17:80/api/auth/login/google')
+      .get('/api/auth/login/google')
       .then(function (response) {
         console.log(response);
       })
