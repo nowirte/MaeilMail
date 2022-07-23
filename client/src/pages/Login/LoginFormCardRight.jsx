ffafc8b97a02d42091b3c49c830487ca7b32f22c
[SW2/서울/2반] 이주혁 <bjuhyeok@gmail.com>
2022-07-23 03:45:05 +0000

    update urls

--- client/src/pages/Login/LoginFormCardRight.jsx
+++ client/src/pages/Login/LoginFormCardRight.jsx
@@ -49,7 +49,7 @@ const LoginForm = props => {
     const bodyData = JSON.stringify(data);
 
     await axios
-      .post('http://localhost:3001/api/auth/login', bodyData, {
+      .post('/api/auth/login', bodyData, {
         headers: {
           'Content-Type': 'application/json',
         },
@@ -69,7 +69,7 @@ const LoginForm = props => {
 
     //CORS에러 있음
     await axios
-      .get('http://localhost:3001/api/auth/login/google')
+      .get('/api/auth/login/google')
       .then(function (response) {
         console.log(response);
       })
