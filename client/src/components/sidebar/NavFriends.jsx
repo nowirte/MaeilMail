3d4830ba77c94952b43067851e843ba699e63fbe
jaeyeong815 <jae0ee@naver.com>
2022-07-27 15:58:18 +0900

    [#7] style: 코드 정리(옵셔널 처리 되어있어서 조건문 삭제)

--- client/src/components/sidebar/NavFriends.jsx
+++ client/src/components/sidebar/NavFriends.jsx
@@ -28,19 +28,18 @@ const NavFriendsArea = () => {
   console.log(friends);
   return (
     <Friends>
-      {friends &&
-        friends?.map(friend => {
-          return (
-            <StyledLink to={`/friend/${friend.user_id}`} key={friend.user_id}>
-              <FriendsList>
-                <div className="profileImgArea">
-                  <img src={friend.profileImage} alt="friendImg" />
-                </div>
-                <span>{friend.nickname}</span>
-              </FriendsList>
-            </StyledLink>
-          );
-        })}
+      {friends?.map(friend => {
+        return (
+          <StyledLink to={`/friend/${friend.user_id}`} key={friend.user_id}>
+            <FriendsList>
+              <div className="profileImgArea">
+                <img src={friend.profileImage} alt="friendImg" />
+              </div>
+              <span>{friend.nickname}</span>
+            </FriendsList>
+          </StyledLink>
+        );
+      })}
     </Friends>
   );
 };
