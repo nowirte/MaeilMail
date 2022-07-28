9c39711ca630a08b4b6161308bdd5f58120175a2
ouivve <ouivve@gmail.com>
2022-07-29 02:14:27 +0900

    [#13] feat: 편지 작성 중 임시저장 기능 추가

--- client/src/pages/Friend/LetterEditor/index.jsx
+++ client/src/pages/Friend/LetterEditor/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import style from './LetterEditor.module.css';
 import CloseIcon from '@mui/icons-material/Close';
 import { useParams } from 'react-router-dom';
@@ -7,15 +7,12 @@ import { getDistance, getTime } from '../utils';
 const LetterEditor = ({ handleWrite, postLetter, user, friend }) => {
   const friendId = useParams().id;
   const contentInput = useRef();
-  const [state, setState] = useState({
-    content: '',
-  });
+  const [content, setContent] = useState('');
+  const [saveTemporarily, setSaveTemporarily] = useState(false);
+  const [savedContent, setSavedContent] = useState(null);
 
   const handleChangeState = e => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
+    setContent(e.target.value);
   };
 
   // 편지 작성
@@ -46,38 +43,81 @@ const LetterEditor = ({ handleWrite, postLetter, user, friend }) => {
     postLetter(newLetter);
   };
 
+  // 보내기 버튼 클릭
   const handleSubmit = e => {
     e.preventDefault();
 
-    if (state.content.length < 1) {
+    if (content.length < 1) {
       // alert('편지 내용을 입력해주세요.');
       contentInput.current.focus();
       return;
     }
     const id = friendId;
-    // onCreate([state.content, id]);
-    createHandler([state.content, id]);
+    createHandler([content, id]);
     handleWrite();
-    setState({
-      content: '',
-    });
+    setContent('');
   };
+
+  // 임시저장 버튼 클릭
+  const handleTemporaryStore = () => {
+    setSaveTemporarily(true);
+    setTemporaryContent();
+    handleWrite(false);
+  };
+
+  // 임시저장 유무 확인
+  const checkTemporaryContent = async () => {
+    const content = await localStorage.getItem(friendId);
+    if (content === null) setSaveTemporarily(false);
+    else {
+      setSavedContent(content);
+      setSaveTemporarily(true);
+    }
+    return;
+  };
+
+  // 임시저장 저장하기
+  const setTemporaryContent = () => {
+    localStorage.setItem(friendId, content);
+    return;
+  };
+
+  // 임시저장 불러오기
+  const getTemporaryContent = () => {
+    setContent(savedContent);
+    setSaveTemporarily(false);
+  };
+
+  useEffect(() => {
+    checkTemporaryContent();
+  }, []);
+
   return (
     <div className={style.LetterEditor}>
       <div className={style.FlexBox}>
-        <CloseIcon
-          onClick={handleWrite}
-          style={{ width: '2rem', padding: '1rem .5rem', cursor: 'pointer' }}
-        />
-        <div className="btn" onClick={handleSubmit}>
-          보내기
+        <span className={style.Btn}>
+          <CloseIcon onClick={handleWrite} style={{ width: '2rem' }} />
+        </span>
+        <div>
+          {saveTemporarily === false ? (
+            <span className={style.Btn} onClick={handleTemporaryStore}>
+              임시저장
+            </span>
+          ) : (
+            <span className={style.Btn} onClick={getTemporaryContent}>
+              불러오기
+            </span>
+          )}
+          <span className={style.Btn} onClick={handleSubmit}>
+            보내기
+          </span>
         </div>
       </div>
       <div>
         <textarea
           ref={contentInput}
           name="content"
-          value={state.content}
+          value={content}
           onChange={handleChangeState}
           placeholder="탭하여 편지 쓰기..."
         />
