43677f9aeabee17928721bb651033de27a1c0657
JuHyeokLee <bjuhyeok@gmail.com>
2022-08-02 15:50:19 +0900

    fix: url경로 수정

--- client/src/pages/Signup/userLocationFunction.js
+++ client/src/pages/Signup/userLocationFunction.js
@@ -5,7 +5,6 @@ export default async function getLocInfo() {
     const res = await axios(
       `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.REACT_APP_LOCATION_API_KEY}`
     );
-    console.log(res);
     const { latitude, longitude, country_name } = res.data;
 
     return { location: country_name, latitude: latitude, longitude: longitude };
